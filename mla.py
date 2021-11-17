49f92f7d7cc2ec925af4000735db1b186da23c9c
SquarishRectangle <61360928+SquarishRectangle@users.noreply.github.com>
2021-11-16 22:43:46 -0500

    v0.1
    Currently only support MLA 9th Edition

---
+++ mla.py
@@ -0,0 +1,351 @@
+from tools import *
+import nullabledate
+
+
+#get the author and title since it's the same for almost all the things
+def authorTitle(subjectName):
+    noAuthor = False
+    #rawAuthors
+    rawAuthors = getAuthors("mla")
+    if rawAuthors[0]:
+        #first author
+        rawAuthors[0] = rawAuthors[0].split()
+        #lastname
+        lastname = rawAuthors[0][-1]
+        authors = lastname + ", "
+        del rawAuthors[0][-1]
+        #first and middle names
+        for name in rawAuthors[0]:
+            authors += name + ' '
+        del rawAuthors[0]
+
+        #other authors
+        if rawAuthors:
+            if rawAuthors[0] == "et al.":
+                authors += "et al. "
+                ILC = "(" + lastname + " et al.)"
+            else:
+                authors += "and " + rawAuthors[0]
+                ILC = "(" + lastname + ", and " + rawAuthors[0].split()[-1] + ")"
+        else:
+            ILC = "(" + lastname + ")"
+            authors = authors[:-1]
+
+        authors += ". "
+    else:
+        noAuthor = True
+        authors = ""
+
+
+    #title
+
+    title = inp("Enter the title of the " + subjectName)
+    #create effective title (used for alphabetizing)
+    eTitle = ""
+    delChars = 0
+
+    for word in title.split():
+        if not word.lower() in ("a", "an", "the"):
+            eTitle += word + " "
+
+    if noAuthor:
+        ILC = eTitle.split()[0]
+
+    return [authors, title, eTitle, ILC]
+
+
+#get publication since it's the same across many modes
+def publication(subjectName, italicize=False):
+    name = inp("Enter the " + subjectName)
+    if italicize:
+        name = '*' + name + '*'
+    return name + ", "
+
+
+#get the database and URL since it's also common across many modes
+def databaseURL(italicize=False):
+    db = inp("Enter database (enter 'ndb' or 'no database' if database is missing)")
+    #return none since there isn't an url either if theres no database
+    if db == "ndb" or db == "no database":
+        return None
+
+    #properly italisize if required
+    if italicize:
+        db = '*' + db + "*, "
+
+    #get url
+    url = inp("Enter URL") + ". "
+    return [db, url]
+
+
+#journal section
+def journal():
+    #get the author and title
+    aT = authorTitle("article")
+
+    authors = aT[0]
+    title = "\"" + aT[1] + "\" "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get the journal name
+    journalName = publication("title of the journal", True)
+
+    #get volume and issue numbers
+    volume = "vol. " + inp("Enter volume number") + ', '
+    issue = inp("Enter issue number")
+    if issue:
+        issue = "no. " + issue + ', '
+
+    #get date. This isn't a function since it's subtly different across different modes
+    while True:
+        date = getDate()
+        if date.year or date.nodate:
+            break
+        else:
+            print ("Missing year")
+
+    strDate = ""
+    if not date.nodate:
+        if date.year:
+            strDate = str(date.year) + ', '
+            if date.month:
+                strDate = date.strMonth + ' ' + strDate
+                if date.day:
+                    strDate = str(date.day) + ' ' + strDate
+
+    #get page numbers
+    page = ""
+    pg = getPages()
+    if pg:
+        page = "pp. " + str(pg[0]) + '-' + str(pg[1]) + ". "
+
+    #get database and url
+    dburl = databaseURL(True)
+    db = ""
+    url = ""
+    if dburl:
+        db = dburl[0]
+        url = dburl[1]
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + journalName + volume + issue + strDate + page + db + url
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
+
+
+#news article
+def news():
+    #get the author and title
+    aT = authorTitle("news article")
+
+    authors = aT[0]
+    title = "\"" + aT[1] + "\" "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get publisher name
+    publisher = publication("publisher", True)
+
+    #get date. This isn't a function since it's subtly different across different modes
+    while True:
+        date = getDate()
+        if date.year or date.nodate:
+            break
+        else:
+            print ("Missing year")
+
+    strDate = ""
+    if not date.nodate:
+        if date.year:
+            strDate = str(date.year) + ', '
+            if date.month:
+                strDate = date.strMonth + ' ' + strDate
+                if date.day:
+                    strDate = str(date.day) + ' ' + strDate
+
+    #get url
+    url = inp("Enter URL") + ". "
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + publisher + strDate + url
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
+
+
+#physically printed books
+def printBook():
+    #get author and title
+    aT = authorTitle("book")
+
+    authors = aT[0]
+    title = '*' + aT[1] + "*. "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get edition number
+    edition = ""
+    while True:
+        edition = inp("Enter edition number (if there are any)")
+        if not edition:
+            break
+        try:
+            edition = int(edition)
+            if edition == 1:
+                edition = "1st"
+                break
+            elif edition == 2:
+                edition = "2nd"
+                break
+            else:
+                edition = str(edition) + "th"
+                break
+        except:
+            print ("Please enter a valid number")
+
+    if edition:
+        edition += " ed., "
+
+    #get publisher
+    publisher = publication("publisher")
+
+    #get date. This isn't a function since it's subtly different across different modes
+    while True:
+        date = getDate()
+        if date.year or date.nodate:
+            break
+        else:
+            print ("Missing year")
+
+    strDate = ""
+    if not date.nodate:
+        if date.year:
+            strDate = str(date.year) + '. '
+            if date.month:
+                strDate = date.strMonth + ' ' + strDate
+                if date.day:
+                    strDate = str(date.day) + ' ' + strDate
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + edition + publisher + strDate
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
+
+
+#eBooks
+def eBook():
+    #get author and title
+    aT = authorTitle("book")
+
+    authors = aT[0]
+    title = '*' + aT[1] + "*, "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get editors
+    editors = "edited by " + inp("Enter editor names") + ". "
+
+    #get publisher
+    publisher = publication("publisher")
+
+    #get date. This isn't a function since it's subtly different across different modes
+    while True:
+        date = getDate()
+        if date.year or date.nodate:
+            break
+        else:
+            print ("Missing year")
+
+    strDate = ""
+    if not date.nodate:
+        if date.year:
+            strDate = str(date.year) + '. '
+            if date.month:
+                strDate = date.strMonth + ' ' + strDate
+                if date.day:
+                    strDate = str(date.day) + ' ' + strDate
+
+    #get database and url
+    dburl = databaseURL(True)
+    db = ""
+    url = ""
+    if dburl:
+        db = dburl[0]
+        url = dburl[1]
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + editors + publisher + strDate + db + url
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
+
+
+#specific chapters in books
+def chapter():
+    #get author and title of the chapter
+    aT = authorTitle("chapter")
+
+    authors = aT[0]
+    title = '\"' + aT[1] + "\" "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get the book name
+    book = publication("name of the book", True)
+
+    #get the editors
+    editors = "edited by " + inp("Enter editor names") + ", "
+
+    #get the publisher
+    publisher = publication("publisher")
+
+    #get date. This isn't a function since it's subtly different across different modes
+    while True:
+        date = getDate()
+        if date.year or date.nodate:
+            break
+        else:
+            print ("Missing year")
+
+    strDate = ""
+    if not date.nodate:
+        if date.year:
+            strDate = str(date.year) + '. '
+            if date.month:
+                strDate = date.strMonth + ' ' + strDate
+                if date.day:
+                    strDate = str(date.day) + ' ' + strDate
+
+    #get page number
+    page = ""
+    pg = getPages()
+    if pg:
+        page = "pp. " + str(pg[0]) + '-' + str(pg[1]) + ". "
+
+    #get database and url
+    dburl = databaseURL(True)
+    db = ""
+    url = ""
+    if dburl:
+        db = dburl[0]
+        url = dburl[1]
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + book + editors + publisher + strDate + page + db + url
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
+
+
+#web pages
+def web():
+    #get author and title
+    aT = authorTitle("chapter")
+
+    authors = aT[0]
+    title = '\"' + aT[1] + "\" "
+    eTitle = aT[2]
+    ILC = aT[3]
+
+    #get publisher
+    publisher = publication("publisher", True)
+
+    #get url
+    url = inp("Enter URL") + ". "
+
+    #create the citation and file containing the proper citation, effective title and in line citation
+    citation = authors + title + publisher + url
+    createFile("Citations/MLA", authors[:-2], [eTitle, ILC, citation])
---
+++ tools.py
@@ -0,0 +1,110 @@
+import os
+import copy
+import nullabledate
+
+#just some of my standard functions that i use everywhere
+def clone(var):
+    return copy.deepcopy(var)
+
+#just some of my standard functions that i use everywhere
+def inp(msg, lower=False):
+    usr = input(msg + "\n$ ")
+    if usr:
+        while usr[-1] == ' ':
+            usr = usr[:-1]
+    if lower:
+        usr = usr.lower()
+    return usr
+
+#create and write to a file
+def createFile(path, filename, contents=None):
+    #create directory if not exists
+    while not os.path.exists(path):
+        os.makedirs(path)
+
+    #add the path together
+    fPath = path + '/' + filename
+
+    #create the file. (if it already exists append an integer to the end of file name)
+    i = 2
+    if os.path.exists(fPath):
+        fPath += str(i)
+        while os.path.exists(fPath):
+            i += 1
+            fPath = path + filename + str(i)
+
+    file = open(fPath + ".txt", 'a')
+    #write to file if needed
+    if contents:
+        for c in contents:
+            file.write(c + "\n\n")
+    file.close()
+    print ("Your citation has been generated to " + fPath + ".txt")
+
+
+#get author function (it lives here because I originally wanted to do APA as well as MLA but i ran out of time)
+def getAuthors(cFormat):
+    authors = []
+    #if the format is mla which it always is :/
+    if cFormat == "mla":
+        #while loop to make sure a correct input is returned
+        while True:
+            numAuthors = inp("How many authors are there? ")
+            try:
+                numAuthors = int(numAuthors)
+            except Exception:
+                numAuthors = -1
+
+            if numAuthors == 0:
+                authors.append('')
+                return authors
+            elif numAuthors == 1:
+                a = inp("Enter author name")
+                authors.append(a)
+                return authors
+            elif numAuthors == 2:
+                a = inp("Enter primary author name")
+                authors.append(a)
+                a = inp("Enter secondary author name")
+                authors.append(a)
+                return authors
+            elif numAuthors > 2:
+                a = inp ("Enter primary author name")
+                authors.append(a)
+                authors.append("et al")
+                return authors
+            else:
+                print ("Please enter a valid number of authors")
+
+#get date function returns a nullabledate.Date data type
+def getDate():
+    usrDate = inp("Enter date (enter 'nd' or 'no date' if date is missing)")
+    return nullabledate.parser(usrDate)
+
+#get pages. returns a list of starting and ending page
+def getPages():
+    #get starting page
+    while True:
+        pgs = inp("Enter start page (enter 'np' or 'no page' if page numbers are missing)")
+        if pgs == "np" or pgs == "no page":
+            return None
+
+        try:
+            pgs = int(pgs)
+            break
+        except:
+            print ("Enter a valid number")
+
+    #get ending page
+    while True:
+        pge = inp("Enter end page (enter 'np' or 'no page' if page numbers are missing)")
+        if pge == "np" or pge == "no page":
+            return None
+
+        try:
+            pge = int(pge)
+            break
+        except:
+            print ("Enter a valid number")
+
+    return [pgs, pge]
